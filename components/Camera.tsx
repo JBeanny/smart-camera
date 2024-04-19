24e09ebaa7c2fd0a63cc104ce6c3a46efe48159a
JBeanny <yimsotharoth999@gmail.com>
2024-04-19 10:36:11 +0700

    feat: change lens mode

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -8,6 +8,7 @@ export const Camera = () => {
   const [photo, setPhoto] = useState(null);
   const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
   const [facingMode, setFacingMode] = useState("user");
+  const [lensMode, setLensMode] = useState<string>("none");
   const [videoDimensions, setVideoDimensions] = useState({
     width: "auto",
     height: "auto",
@@ -23,7 +24,7 @@ export const Camera = () => {
       const constraints = {
         video: {
           facingMode: facingMode,
-          resizeMode: 1,
+          resizeMode: lensMode,
         },
       };
 
@@ -40,22 +41,59 @@ export const Camera = () => {
           console.log("An error occurred: " + err);
         });
     }
-  }, [facingMode]);
+  }, [facingMode, lensMode]);
+
+  const changeLensMode = (lens: string) => {
+    let newLensMode = "none"; // Default to x1 lens mode
+    switch (lens) {
+      case "x0.5":
+        newLensMode = "x0.5";
+        break;
+      case "x2":
+        newLensMode = "x2";
+        break;
+      case "none":
+        newLensMode = "none"; // Cycle back to x1 lens mode
+        break;
+      default:
+        newLensMode = "none";
+        break;
+    }
+    setLensMode(newLensMode);
+  };
 
   return (
     <div className="flex justify-center items-center flex-col gap-4 w-[80%] m-auto">
-      <h1 className="text-center">{cameraOpen}</h1>
+      <h1 className="text-center text-white">{cameraOpen}</h1>
       <video
         ref={videoRef}
         autoPlay
         className={`w-${videoDimensions.width} h-${videoDimensions.height} border-2 border-white rounded-lg object-cover`}></video>
+
+      <div className="flex gap-4 justify-center items-center">
+        <button
+          onClick={() => changeLensMode("x0.5")}
+          className="bg-white text-gray p-2 rounded-full w-[40px] h-[40px] text-sm active:scale-110">
+          0.5
+        </button>
+        <button
+          onClick={() => changeLensMode("none")}
+          className="bg-white text-gray p-2 rounded-full w-[40px] h-[40px] text-sm active:scale-110">
+          1
+        </button>
+        <button
+          onClick={() => changeLensMode("x2")}
+          className="bg-white text-gray p-2 rounded-full w-[40px] h-[40px] text-sm active:scale-110">
+          2
+        </button>
+      </div>
 
       <div className="w-full flex flex-col justify-between gap-4">
         <Button
           onClick={() =>
             camera.toggleCamera({ videoStream, setFacingMode, facingMode })
           }
-          className="bg-white text-gray"
+          className="bg-white text-gray active:scale-110"
           text="Rotate Camera"
         />
         <Button
@@ -67,7 +105,7 @@ export const Camera = () => {
               setVideoDimensions,
             })
           }
-          className="bg-white text-gray"
+          className="bg-white text-gray active:scale-110"
           text="Take Photo 📸"
         />
         {photo && (
@@ -77,7 +115,7 @@ export const Camera = () => {
                 camera.downloadPhoto({ photo });
               }}
               disable={!photo}
-              className="bg-green text-white"
+              className="bg-green text-white active:scale-110"
               text="Download Photo"
             />
             <Button
@@ -85,7 +123,7 @@ export const Camera = () => {
                 camera.discardPhoto({ setPhoto, setVideoDimensions })
               }
               disable={!photo}
-              className="bg-red text-white"
+              className="bg-red text-white active:scale-110"
               text="Discard"
             />
           </>
