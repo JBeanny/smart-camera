985ee02acb0b90e22a0ed8f6df225e8d359e1f77
JBeanny <yimsotharoth999@gmail.com>
2024-04-23 09:48:10 +0700

    feat: configure pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,17 @@ export const metadata: Metadata = {
   title: "Sloth Camera By Bean",
   description:
     "Smart Camera which sends the taken pictures to specific telegram channel",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [{ name: "Sotharoth Yim" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/apple-touch-icon.png" },
+    { rel: "icon", url: "/favicon.ico" },
+  ],
 };
 
 export default function RootLayout({
